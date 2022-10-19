46c915a4853509b34af4935d2afae6b90af56295
Michal Jeszko <michal.jeszko@sobre.pl>
2022-10-19 14:42:31 +0200

    feat: view card-normal

--- src/components/LinkTo/LinkTo.js
+++ src/components/LinkTo/LinkTo.js
@@ -2,12 +2,12 @@ import React from "react";
 import classnames from "classnames";
 import { Link } from "react-router-dom";
 
-const LinkTo = ({ customClass, link, text }) => {
+const LinkTo = ({ customClass, link, children }) => {
   const cssStyle = classnames(customClass);
 
   return (
     <Link to={link} className={cssStyle}>
-      {text}
+      {children}
     </Link>
   );
 };
--- src/components/Products-list-view/Card-normal/CardNormal.js
+++ src/components/Products-list-view/Card-normal/CardNormal.js
@@ -1,12 +1,68 @@
-import React from 'react'
+import React from "react";
+import LinkTo from "../../LinkTo/LinkTo";
 
-const CardNormal = (props) => {
+import "./CardNormal.scss";
 
-  console.log(props)
-
+const CardNormal = ({
+  title,
+  description,
+  price,
+  stock,
+  discountPercentage,
+  rating,
+  category,
+  thumbnail,
+  images,
+}) => {
   return (
-    <div>CardNormal</div>
-  )
-}
+    <div className="columns border-bottom card-normal__wrapper">
+      <LinkTo customClass="column is-2 box-square">
+        <img src={thumbnail} alt={title} className="image-cover" />
+      </LinkTo>
+      <div className="column is-flex is-flex-direction-column is-justify-content-center">
+        <LinkTo>
+          <h2 className="title is-5 is-uppercase has-text-weight-semibold">
+            {title}
+          </h2>
+        </LinkTo>
+        <LinkTo>
+          <p className="subtitle is-6">{description}</p>
+        </LinkTo>
+      </div>
+      <div className="column is-2 is-flex is-flex-direction-column is-justify-content-center">
+        <h3 className="title is-5 mb-0 has-text-centered">{price}</h3>
+        <p className="has-text-weight-light has-text-centered">{price}</p>
+        <p className="has-text-weight-light has-text-centered">netto</p>
+      </div>
+      <div className="column is-1 is-flex is-flex-direction-column is-justify-content-center">
+        <p className="title is-6">
+          {stock} <i className="fa-solid fa-arrow-up-short-wide"></i>
+        </p>
+      </div>
+      <div class="control is-flex is-flex-direction-row is-justify-content-center is-align-items-center pr-3">
+        <div className="card-normal__input-wrapper is-flex">
+          <input
+            className="input is-hovered border-wrapper"
+            type="text"
+            placeholder="Hovered input"
+            value="1"
+          />
+          <div className="buttons-counter__wrapper is-flex is-justify-content-space-between">
+            <button type="button" className="card-normal__button btn-counter">
+              <i class="fa-solid fa-plus"></i>
+            </button>
+            <button type="button" className="card-normal__button btn-counter">
+              <i class="fa-solid fa-minus"></i>
+            </button>
+          </div>
+          <p className="title is-size-7 mx-1 my-auto">szt.</p>
+          <button type="button" className="button is-primary px-3">
+            <i class="fa-solid fa-cart-arrow-down"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CardNormal
+export default CardNormal;
