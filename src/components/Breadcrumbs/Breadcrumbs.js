1cc7e358de6fe3d0506917c63b19c43635ba8d6b
Michal Jeszko <michal.jeszko@sobre.pl>
2022-11-03 20:42:40 +0100

    fix: breadcrumbs

--- src/components/Breadcrumbs/Breadcrumbs.js
+++ src/components/Breadcrumbs/Breadcrumbs.js
@@ -1,23 +1,23 @@
 import React from "react";
 import LinkTo from "../LinkTo/LinkTo";
-import useBreadcrumbs from 'use-react-router-breadcrumbs';
+import useBreadcrumbs from "use-react-router-breadcrumbs";
 import classNames from "classnames";
 
 const Breadcrumbs = ({ customClass }) => {
   const breadcrumbs = useBreadcrumbs();
-  const cssStyles = classNames(customClass)
+  const cssStyles = classNames(customClass);
 
   return (
-    <p className={cssStyles}>
+    <div className={cssStyles}>
       {breadcrumbs.map(({ breadcrumb, match }, index) => (
-        <>
-          <LinkTo key={match.pathname} link={match.pathname}>
-            {breadcrumb}
-          </LinkTo>
-          {index !== breadcrumbs.length - 1 && <i class="fa-solid fa-angle-right mx-2"></i>}
-        </>
+        <p key={match.pathname} className="is-inline-block">
+          <LinkTo link={match.pathname}>{breadcrumb}</LinkTo>
+          {index !== breadcrumbs.length - 1 && (
+            <i className="fa-solid fa-angle-right mx-2"></i>
+          )}
+        </p>
       ))}
-    </p>
+    </div>
   );
 };
 
